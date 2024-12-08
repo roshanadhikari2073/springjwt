b92712e6923daca776cfcbba53670ec1c3cc2f83
roshanadhikari <greenboyroshan@gmail.com>
2024-12-08 01:51:44 -0500

    Initialized the maven project for the jwt testing

---
+++ src/main/java/com/tahoe/jwt/tahoe_jwt/TahoeJwtApplication.java
@@ -0,0 +1,13 @@
+package com.tahoe.jwt.tahoe_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TahoeJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TahoeJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tahoe/jwt/tahoe_jwt/TahoeJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tahoe.jwt.tahoe_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TahoeJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
